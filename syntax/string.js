27ff5335bf28e242de1e7b9737b5f85ad22c324a
Kimhyuntae9665 <Kimhyuntae9665>
2023-05-29 11:59:11 +0900

    Template Literal

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+console.log('1'+"66");
+var str = 'dpfkngdkfmgldfg gokmgsplf,[le,f[      fpweofmw[epf,[elfpkmfpm/c,x,';
+console.log(str.length); 
---
+++ syntax/template.js
@@ -0,0 +1,10 @@
+var name = 'egoing';
+
+var letter = `lNostrud exercitation in in ${name} 
+
+reprehenderit.  Incididunt esse duis non duis nisi sunt  ${name}  
+
+consectetur egoing officia sunt. Consequat ea ad officia eu cupidatat dolor commodo qui eiusmod commodo sint. Ullamco do consectetur officia nulla elit.`
+
+
+console.log(letter);
